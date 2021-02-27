678c300ae0073505d6a6d7fad72bc2cca74e0afe
lazafi <lazafi@gmail.com>
2021-02-27 11:53:06 +0100

    js config

--- www/src/config.default.js
+++ www/src/config.default.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "AWS_USER_POOLS_WEB_CLIENT_ID",     // CognitoClientID
-  "api_base_url": "API_BASE_URL",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "COGNITO_HOSTED_DOMAIN",                   // CognitoDomainName
-  "redirect_url": "REDIRECT_URL"                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "5t0s6ognl7p4fmmu5nb3aslnaf",     // CognitoClientID
+  "api_base_url": "https://w9zch31qi7.execute-api.eu-central-1.amazonaws.com/prod",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-sandbox-sam-todoex.auth.eu-central-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1ziayy6qn25zi.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
